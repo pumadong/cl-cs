953b63a9663a9708f05c6e4e075451b1d2f3c0d2
PumaDong <puma.dong@hotmail.com>
2014-09-17 14:22:03 +0800

    项目结构

---
+++ cs-data/src/main/java/com/cl/cs/mapper/base/DictionaryMapper.java
@@ -0,0 +1,17 @@
+package com.cl.cs.mapper.base;
+
+import com.cl.cs.model.base.Dictionary;
+
+public interface DictionaryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Dictionary record);
+
+    int insertSelective(Dictionary record);
+
+    Dictionary selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Dictionary record);
+
+    int updateByPrimaryKey(Dictionary record);
+}
